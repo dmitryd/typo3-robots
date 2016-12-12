7e2ebbacedc388d10bbcee0c345bf8db69fb961a
Daniel Schledermann <daniel@linkfactory.dk>
2016-12-12 15:45:07 +0100

    [FEATURE] Make recursion work for new pages also

--- Classes/Hooks/Tcemain.php
+++ Classes/Hooks/Tcemain.php
@@ -6,13 +6,31 @@ namespace Dschledermann\Robots\Hooks;
 class Tcemain {
 
 	function processDatamap_afterDatabaseOperations($status, $table, $id, &$fieldArray, &$pObj) {
-		if($table == 'pages'  && isset($fieldArray['tx_robots_flags']) && $status == 'update' ){
+		// If a page is updated and the robot flags are on
+		if ($table == 'pages' && isset($fieldArray['tx_robots_flags']) && $status == 'update' ) {
+
+			// Apply to all subpages
 			if($fieldArray['tx_robots_flags'] & 4){
 				$updateArray = array('tx_robots_flags' => $fieldArray['tx_robots_flags']);
 			} else {
 				$updateArray = array('tx_robots_flags' => 0);
 			}
 			$this->updateChilds($id,$updateArray);
+		}
+	}
+
+	public function processDatamap_preProcessFieldArray(&$incomingFieldArray, $table, $id, &$parentObj) {
+
+		// A new page should inherit a parent page's setting if the parents inherit checkbox is marked
+		if ($table == 'pages' && stripos($id, 'new') !== false) {
+			global $TYPO3_DB;
+
+			$rs = $TYPO3_DB->exec_SELECTquery('tx_robots_flags', 'pages', 'uid = ' . $incomingFieldArray['pid']);
+			list($robots_flags) = $TYPO3_DB->sql_fetch_row($rs);
+
+			if ($robots_flags & 4) {
+				$incomingFieldArray['tx_robots_flags'] = $robots_flags;
+			}
 		}
 	}
 
